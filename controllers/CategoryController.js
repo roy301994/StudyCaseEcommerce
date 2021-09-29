3a1c97a63db1090b353478bc56e6136c8eb5c8d5
roy301994 <tristantofir@gmail.com>
2021-09-30 06:33:08 +0700

    feature findAll,findOne & Destroy by myself

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -50,7 +50,6 @@ class CategoryController {
             where: { id: req.params.id },
           }
         );
-
         // req._data = Category;
         req._status = 201;
         req._error = false;
@@ -68,6 +67,105 @@ class CategoryController {
       next(new Error(error));
     }
   };
+
+  DeleteCategory = async (req, res, next) => {
+    try {
+      const verify = await CategoryModel.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (verify) {
+        await CategoryModel.destroy({
+          where: {
+            id: req.params.id,
+          },
+        });
+        req._status = 200;
+        req._error = false;
+        req._message = "Data has been deleted";
+      } else {
+        req._status = 501;
+        req._error = true;
+        req._message = "Data not found";
+      }
+      next();
+    } catch (error) {
+      req._status = 501;
+      req._error = true;
+      req._message = error;
+      next(new Error(error));
+    }
+  };
+
+  FindOneCategory = async (req, res, next) => {
+    try {
+      const check = await CategoryModel.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (check) {
+        const category = await CategoryModel.findOne({
+          where: {
+            id: req.params.id,
+          },
+        });
+        req._data = category;
+        req._status = 200;
+        req._error = false;
+        req._message = "Data has been found";
+      } else {
+        req._status = 501;
+        req._error = true;
+        req._message = "Data not found";
+      }
+
+      next();
+    } catch (error) {
+      req._status = 501;
+      req._error = true;
+      req._message = error;
+      next(new Error(error));
+    }
+  };
+
+
+    FindAllCategory = async (req,res,next)=>{
+
+      try {
+        const category= await CategoryModel.findAll()
+        req._data=category
+        req._status=200
+        req._error=false
+        req._message="List all data Category"
+        next()
+      } catch (error) {
+        req._status=501
+        req._error=true
+        req._message=error
+        next(new Error(error))
+      }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 module.exports = new CategoryController();
--- routes/category.js
+++ routes/category.js
@@ -7,9 +7,11 @@ const responeMiddleware = require("../middleware/respone");
 const AuthMiddleware = require("../middleware/authentikasi");
 
 router.use(appkeyMiddleware);
-router.post("/",AuthMiddleware,CategoryController.createCategory)
-router.put("/:id",AuthMiddleware,CategoryController.UpdateCategory)
+router.post("/", AuthMiddleware, CategoryController.createCategory);
+router.put("/:id", AuthMiddleware, CategoryController.UpdateCategory);
+router.delete("/:id", AuthMiddleware, CategoryController.DeleteCategory);
+router.get("/", AuthMiddleware, CategoryController.FindAllCategory);
+router.get("/:id", AuthMiddleware, CategoryController.FindOneCategory);
 router.use(responeMiddleware);
-
 
 module.exports = router;
